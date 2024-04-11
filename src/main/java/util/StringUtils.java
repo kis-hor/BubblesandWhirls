d03fec739234493938c2bea86aff556e0759fdc4
Sharia <shariaali46@gmail.com>
2024-04-12 00:05:03 +0545

    added user details in string utils

--- src/main/java/util/StringUtils.java
+++ src/main/java/util/StringUtils.java
@@ -1,5 +1,11 @@
 package util;
 
 public class StringUtils {
-
+	public static final String USER_NAME = "username";
+	public static final String FIRST_NAME = "firstName";
+	public static final String LAST_NAME = "lastName";
+	public static final String EMAIL = "email";
+	public static final String PHONE_NUMBER = "phoneNumber";
+	public static final String PASSWORD = "password";
+	public static final String RETYPE_PASSWORD = "retypePassword";
 }
